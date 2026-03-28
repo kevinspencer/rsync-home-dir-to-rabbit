75caee1f4e5833e96b588745d412e9771fc647e7
Kevin Spencer <kevin@kevinspencer.org>
2026-03-28 00:08:06 -0400

    defensive check if mount exists

--- rsync-home-dir-to-rabbit.sh
+++ rsync-home-dir-to-rabbit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "/Volumes/rabbit" ]; then
+    echo "rabbit is not connected. Exiting."
+    exit 1
+fi
+
 rsync -avh --delete -E \
   --exclude=".DS_Store" \
   --exclude="Library/" \
